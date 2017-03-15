4f921c1d4ba69be52923520fa2cb1300c8b83b9e
Blaise Gauvin St-Denis <st-denis.blaise@ouranos.ca>
2017-03-15 16:31:57 -0400

    Add development version of cfsr conversion tools

---
+++ cfs/cfsr_grib2_to_netcdf_parallel.py
@@ -0,0 +1,69 @@
+# ipcluster start -n 10
+
+import os
+from ipyparallel import Client
+
+import cfsr
+
+# For new variables, need to edit the info in cfsr.py
+
+path_data = '/scen3/stdenis/projects/climate_datasets/cfs2'
+path_output = '/scen3/stdenis/projects/climate_datasets/cfs2/nc_files'
+grib_source = 'rda' # Should be 'rda' or 'nomads'
+var_name = 'q2m' # This is the name of the variable in the filename.
+grib_var_name = 'Specific humidity' # This is the name in the grib file, can use
+                           # gribou.all_str_dump(grib_file) to find it.
+grib_level = 2 # If there is no units in the str dump, set to None.
+                  # Otherwise this should be the number before the units.
+nc_var_name = 'huss' # If it is not clear what this is in the CF convention
+                         # table, or the units don't match, use the CFSR name.
+nc_units = '1' # Also available from gribou.all_str_dump(grib_file)
+nc_format = 'NETCDF4_CLASSIC'
+resolution = 'highres' # 'highres' or 'lowres' or 'prmslmidres' or
+                       # 'ocnmidres' or 'ocnlowres'. This is determined
+                       # by the selection of the grid when obtaining the
+                       # files.
+initial_year = 2015 # Usually 1979, sometimes the parallel code does not
+                    # complete the later files, rerun with final years.
+final_year = 2015 # Usually 2010, some ocean files end in 2009.
+cache_size = 100
+
+rc = Client()
+with rc[:].sync_imports(): import sys
+rc[:].execute("sys.path.append('/scen3/stdenis/projects/climate_datasets/cfs2/git/pycfs/cfs')")
+with rc[:].sync_imports(): import cfsr
+with rc[:].sync_imports(): import gribou
+
+lview = rc.load_balanced_view()
+
+mylviews = []
+for yyyy in range(initial_year,final_year+1):
+    for mm in ['01','02','03','04','05','06','07','08','09','10','11','12']:
+    #for mm in ['12']:
+        if resolution in ['highres','prmslmidres','ocnmidres']:
+            if (yyyy > 2011) or ((yyyy == 2011) and (int(mm) > 3)):
+                grib_file = "%s.cdas1.%s%s.grb2" % (var_name,str(yyyy),mm)
+            else:
+                grib_file = "%s.gdas.%s%s.grb2" % (var_name,str(yyyy),mm)
+            warp = "%s_1hr_cfsr_reanalysis_%s%s.nc" % (nc_var_name,str(yyyy),mm)
+            nc_file = os.path.join(path_output,warp)
+        elif resolution in ['lowres','ocnlowres']:
+            grib_file = "%s.l.gdas.%s%s.grb2" % (var_name,str(yyyy),mm)
+            warp = "%s_1hr_cfsr_reanalysis_lowres_%s%s.nc" % (nc_var_name,
+                                                              str(yyyy),mm)
+            nc_file = os.path.join(path_output,warp)
+        grib_file = os.path.join(path_data,grib_file)
+        if not os.path.isfile(grib_file):
+            continue
+        print grib_file
+        mylviews.append(lview.apply(cfsr.hourly_grib2_to_netcdf,grib_file,
+                                    grib_source,nc_file,nc_var_name,
+                                    grib_var_name,grib_level,
+                                    cache_size=cache_size,
+                                    overwrite_nc_units=nc_units,
+                                    nc_format=nc_format))
+
+if nc_var_name in ['tasmin','tasmax']:
+    msg1 = "WARNING: this is a cumulative min/max variable, "
+    msg2 = "need to run cfsr_sampling.py afterwards."
+    print msg1+msg2
---
+++ cfs/cfsr_sampling.py
@@ -0,0 +1,80 @@
+import os
+import glob
+import datetime
+
+import numpy as np
+import netCDF4
+import ouranos.formats.netcdf as nc
+
+variables = ['tasmin','tasmax']
+
+path_output = '/rech2/stdenis/projects/cfs2/nc_sample'
+path_cfsr = '/rech2/stdenis/projects/cfs2/nc_files'
+nc_format = 'NETCDF4_CLASSIC'
+
+sampling_start = 5
+sampling_step = 6
+
+for var_name in variables:
+    nc_files = glob.glob(os.path.join(path_cfsr,var_name,'*'))
+    for i,nc_file in enumerate(nc_files):
+        warp = os.path.basename(nc_file).replace('1hr','6hr')
+        out_file = os.path.join(path_output,warp)
+        ncref = netCDF4.Dataset(nc_file,'r')
+        timeref = ncref.variables['time']
+        nt = timeref.shape[0]/sampling_step
+        
+        now = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+        nc1 = netCDF4.Dataset(out_file,'w',format=nc_format)
+        
+        nc.nc_copy_attrs(ncref,nc1,
+                         includes=[],
+                         excludes=[],
+                         renames={},
+                         defaults={},
+                         appends={'history':"%s: 6 hourly sample." % (now,)})
+        
+        nc.nc_copy_dimensions(ncref,nc1,
+                              includes=[],
+                              excludes=[],
+                              renames={},
+                              defaults={},
+                              reshapes={})
+        
+        nc.nc_copy_variables_structure(ncref,nc1,
+                                       includes=[],
+                                       excludes=[],
+                                       renames={},
+                                       new_dtype={},
+                                       new_dimensions={},
+                                       create_args={'_global':{'zlib':True}})
+        
+        nc.nc_copy_variables_attributes(ncref,nc1,
+                                        includes=[],
+                                        excludes=[],
+                                        renames={},
+                                        attr_excludes={},
+                                        attr_defaults={})
+        
+        nc.nc_copy_variables_data(ncref,nc1,
+                                  includes=[],
+                                  excludes=['time','time_vectors',var_name],
+                                  renames={})
+        
+        timeref = timeref[:]
+        tvsref = ncref.variables['time_vectors']
+        
+        tvsref = tvsref[:,:]
+        varref = ncref.variables[var_name]
+        
+        varref = varref[...]
+        time1 = nc1.variables['time']
+        tvs1 = nc1.variables['time_vectors']
+        var1 = nc1.variables[var_name]
+        
+        time1[:] = timeref[sampling_start::sampling_step]
+        tvs1[:,:] = tvsref[sampling_start::sampling_step,:]
+        var1[...] = varref[sampling_start::sampling_step,...]
+
+        ncref.close()
+        nc1.close()
